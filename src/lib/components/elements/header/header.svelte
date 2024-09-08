47f4224061ff2440729c8db21fdf84bef5f3c066
Gabriel Meireles <64715757+gabrielmeireles@users.noreply.github.com>
2024-09-08 20:38:56 +0200

    Add Shadcnui and components

---
+++ src/lib/components/elements/header/header.svelte
@@ -0,0 +1,16 @@
+<div>
+	<p>Comexstat Visualizer</p>
+</div>
+
+<style>
+	div {
+		width: 100%;
+		padding: 0.5rem;
+
+		display: flex;
+		background-color: var(--card-foreground);
+		box-shadow: 0 0 0 1px var(--card-background);
+
+		@apply border-b;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Header from '$lib/components/elements/header/header.svelte';
+	import '../app.css';
+</script>
+
+<main>
+	<Header />
+	<slot></slot>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+
+		@apply min-h-screen;
+
+		background-color: var(--background);
+		color: var(--foreground);
+	}
+</style>
